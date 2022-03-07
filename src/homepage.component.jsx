08234cdca40accddc20d64f6892c7650d286c0b8
miken patel <mikenpatel001@gmail.com>
2022-03-08 02:04:15 +0530

    created Homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+export const Homepage=()=>(
+<div className='homepage '>
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Hats</h1>
+                <span className='subtitle'>ShopNow</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Jacket</h1>
+                <span className='subtitle'>ShopNow</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>snikkers</h1>
+                <span className='subtitle'>ShopNow</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Men</h1>
+                <span className='subtitle'>ShopNow</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Women</h1>
+                <span className='subtitle'>ShopNow</span>
+            </div>
+        </div>
+    </div>
+</div>
+);
